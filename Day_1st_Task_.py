c899545cf83c684d58892fa628c436973e2f7509
Vaishnavee Lilhare <98232986+vaishu38986@users.noreply.github.com>
2026-04-22 16:09:30 +0530

    Created using Colab

---
+++ Day_1st_Task_.py
@@ -0,0 +1,169 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPUTZ8iw6qs2u162z1W3c7j",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/vaishu38986/python-basics-practice/blob/main/Day_1st_Task_.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "User = int (input(\"enter the even number\"))\n",
+        "if(User % 2==0):\n",
+        "  print(\"this is the even number\")\n",
+        "else:\n",
+        "  print(\"this is the odd number\")"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "a42G8WkSiaPo",
+        "outputId": "f78b6a17-7c4f-46c9-eee5-b0fcf73dbcbb"
+      },
+      "execution_count": 7,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter the even number45\n",
+            "this is the odd number\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "Even/odd checker"
+      ],
+      "metadata": {
+        "id": "jczn8Nn1iigx"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "A = float(input(\"enter the First Number\"))\n",
+        "B = float(input(\"enter the Seacond number\"))\n",
+        "sum = A+B\n",
+        "print(\"The total the vaule is sum\",sum)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "_3oJO-_EioB5",
+        "outputId": "26eae9c5-3d19-4633-d9d2-e5ff7d14e87c"
+      },
+      "execution_count": 9,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter the First Number493\n",
+            "enter the Seacond number2\n",
+            "The total the vaule is sum 495.0\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "Sum of numbers"
+      ],
+      "metadata": {
+        "id": "dcyEtxqhmRpU"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "num1 = int(input(\"enter the first digit\"))\n",
+        "num2 = int(input(\"Enter the Second digit\"))\n",
+        "Sum = num1+num2\n",
+        "multy= num1*num2\n",
+        "Sub = num1-num2\n",
+        "Modulus = num1%num2\n",
+        "divi = num1/num2\n",
+        "print(\"sum of two numbers\",Sum)\n",
+        "print(\"multy of two numbers\",multy)\n",
+        "print(\"sub of two numbers\", Sub)\n",
+        "print(\"divi of two numbers\",Modulus)\n",
+        "print(\"Moudulus of two numbers\",divi)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "qkMR75McnLVs",
+        "outputId": "19864d2d-b8fe-4a48-c008-a470af6ac6a2"
+      },
+      "execution_count": 17,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter the first digit2\n",
+            "Enter the Second digit3\n",
+            "sum of two numbers 5\n",
+            "multy of two numbers 6\n",
+            "sub of two numbers -1\n",
+            "divi of two numbers 2\n",
+            "Moudulus of two numbers 0.6666666666666666\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "Simple calculator"
+      ],
+      "metadata": {
+        "id": "1N_-Yt-VnULI"
+      }
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "FeeYIsNUnRp5"
+      },
+      "execution_count": null,
+      "outputs": []
+    },
+    {
+      "cell_type": "markdown",
+      "source": [],
+      "metadata": {
+        "id": "j88zBjfdnS7Y"
+      }
+    }
+  ]
+}
